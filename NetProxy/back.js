2ec3b1a6bb77ad13856d42b59db830ae131efffc
IxaKhronos <hdszk2002@yahoo.co.jp>
2017-09-02 16:27:11 +0900

    multi start

--- NetProxy/back.js
+++ NetProxy/back.js
@@ -1,15 +1,7 @@
-﻿var mesExtId;
-var hostname;
-var pathPt;
-var extPt;
-
-chrome.storage.local.get(["host","path","ext","mesExtId"],function(items){
-	if(items.host) hostname=items.host;
-	if(items.path) pathPt=items.path;
-	if(items.ext) extPt=items.ext;
-	if(items.mesExtId) mesExtId=items.mesExtId;
+﻿var setData;
+chrome.storage.local.get(["setData"],function(items){
+	if(items.setData) setData=JSON.parse(items.setData)
 })
-
 var ctabUrl;
 chrome.browserAction.onClicked.addListener(function(tab){
 	ctabUrl=tab.url;
@@ -38,24 +30,25 @@ function startNetCheck(id){
 		}
 	})
 }
-
+var tg;
 var request={};
 chrome.debugger.onEvent.addListener(function(src,method,params){
 	if(debugee){
 		switch(method){
 		  case"Network.requestWillBeSent":
 			var $a = $("<a />").attr("href",params.request.url);
-			if($a[0].hostname==hostname){				
-				var path=$a[0].pathname;			
-				var tm=path.split("/");
-				var filename=tm[tm.length-1];
-				var ext=filename.split(".")[1];
-				if(path.match(pathPt) && ext==extPt){
-					chrome.browserAction.setIcon({"path":"32on.png"})
-					request[params.requestId]={
-						url:filename,
-						post:params.request.postData
-					};
+			for(var k in setData){
+				if($a[0].hostname==setData[k].host){
+					var path=$a[0].pathname;			
+					var tm=path.split("/");
+					var filename=tm[tm.length-1];
+					var ext=filename.split(".")[1];
+					if(path.match(setData[k].path) && ext==setData[k].ext){
+						tg=k;
+						chrome.browserAction.setIcon({"path":"32on.png"})
+						request[params.requestId]={url:filename,post:params.request.postData};
+						break;
+					}
 				}
 			}
 			break;
@@ -75,8 +68,8 @@ chrome.debugger.onEvent.addListener(function(src,method,params){
 
 var rs;
 function fcb(req,res){
-	if(mesExtId){
-		chrome.runtime.sendMessage(mesExtId,{
+	if(setData[tg].mesExtId){
+		chrome.runtime.sendMessage(setData[tg].mesExtId,{
 			"request":req,
 			"response":res
 		})
--- NetProxy/option.js
+++ NetProxy/option.js
@@ -1,17 +1,52 @@
+var setData={};
+var ss
 $(function(){
-	chrome.storage.local.get(["host","path","ext","mesExtId"],function(items){
-		console.log(items)
-		if(items.host) $("#host").val(items.host);
-		if(items.path) $("#path").val(items.path);
-		if(items.ext) $("#ext").val(items.ext);
-		if(items.mesExtId) $("#mesExtId").val(items.mesExtId);
+	chrome.storage.local.get(["setData"],function(items){
+		if(items.setData) setData=JSON.parse(items.setData)
+		setSetData();
 	})
-	$(document).on("click","button",function(){
-		chrome.storage.local.set({
+	$(document).on('change',"#patterns",function(){
+		dspSetData();
+	})
+	function setSetData(){
+		$("#patterns").empty();
+		$("#patName").val("");
+		$("#host").val("");
+		$("#path").val("");
+		$("#ext").val("");
+		$("#mesExtId").val("");
+		for(var key in setData)$("#patterns").append($("<option>"+key+"</option>"))
+		dspSetData();
+	}
+	function dspSetData(){
+		var ckey=$("#patterns option:selected").val();
+		if(ckey){
+			$("#patName").val(ckey);
+			$("#host").val(setData[ckey].host);
+			$("#path").val(setData[ckey].path);
+			$("#ext").val(setData[ckey].ext);
+			$("#mesExtId").val(setData[ckey].mesExtId);
+		}
+	}
+	$(document).on("click","#set",function(){
+		setData[$("#patName").val()]=
+		{
 			"host":$("#host").val(),
 			"path":$("#path").val(),
 			"ext":$("#ext").val(),	
 			"mesExtId":$("#mesExtId").val(),	
-		})
+		}
+		chrome.storage.local.set({"setData":JSON.stringify(setData)});
+		setSetData();
+	})
+	$(document).on("click","#del",function(){
+		var ckey=$("#patterns option:selected").val();
+		delete setData[ckey];
+		setSetData();
+	})
+	$(document).on("click","#cl",function(){
+		setData={};
+		chrome.storage.local.remove("setData");
+		setSetData();
 	})
 })
